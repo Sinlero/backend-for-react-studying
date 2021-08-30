8b83e6ec3379f0de7d0004ff34bce53c1856fbdc
Sinlero <wanted1345@gmail.com>
2021-08-30 10:41:05 +0900

    add counter requests to logs

--- src/main/java/App/Controllers/UserController.java
+++ src/main/java/App/Controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @RestController
@@ -22,11 +23,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private AtomicLong counter = new AtomicLong(0);
+
     @GetMapping("/users")
     @ResponseStatus(code = HttpStatus.OK)
     public UsersDTO getUsers(@RequestParam (defaultValue = "1", required = false) @Min(1) int page,
                              @RequestParam(defaultValue = "10", required = false) @Min(1) @Max(100) int limit) {
-        log.info("Request page number: " + page + "; Request limit records: " + limit);
+        log.info("Request page number: " + page + "; Request limit records: " + limit + "; Request number " + counter.incrementAndGet());
         return userService.getUsers(page, limit);
     }
 
