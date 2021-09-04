11caacacbd9947a8b3abdd58cda2308c86a736c0
Sinlero <wanted1345@gmail.com>
2021-09-04 11:10:28 +0900

    create DTO

--- src/main/java/App/DTO/UserProfileDTO.java
+++ src/main/java/App/DTO/UserProfileDTO.java
@@ -1,4 +1,33 @@
 package App.DTO;
 
+import App.Entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserProfileDTO {
+
+    private String name;
+    private String fullName;
+    private String uniqueUrlName;
+    private String photos;
+    private String aboutMe;
+    private String lookingForAJobDescription;
+    private Boolean lookingForAJob;
+    private ContactsDTO contactsDTO;
+
+    public UserProfileDTO(User user) {
+        name = user.getName();
+        fullName =user.getFullName();
+        uniqueUrlName = user.getUniqueUrlName();
+        photos = user.getPhotos();
+        aboutMe = user.getAboutMe();
+        lookingForAJobDescription = user.getLookingForAJobDescription();
+        lookingForAJob = user.getLookingForAJob();
+        contactsDTO = new ContactsDTO(user.getContacts());
+    }
+
 }
