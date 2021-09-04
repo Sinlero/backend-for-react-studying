55aa7a5956b1da3f9a3a80c7a658d16d05a37bce
Sinlero <wanted1345@gmail.com>
2021-09-04 11:11:34 +0900

    add getting profile by user id

--- src/main/java/App/Services/UserService.java
+++ src/main/java/App/Services/UserService.java
@@ -1,22 +1,39 @@
 package App.Services;
 
+import App.DTO.UserDTO;
+import App.DTO.UserProfileDTO;
 import App.DTO.UsersDTO;
+import App.Entities.Contacts;
 import App.Entities.User;
+import App.Exceptions.UserNotFoundException;
 import App.Repositories.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 @Service
 public class UserService {
 
-    @Autowired
-    private UsersRepository usersRepository;
+    private final UsersRepository usersRepository;
+
+    public UserService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
     public UsersDTO getUsers(int page, int limit) {
         Page<User> users = usersRepository.findAll(PageRequest.of(--page, limit, Sort.by("id")));
-        return new UsersDTO(users.getContent(), users.getTotalElements(), users.getTotalPages());
+        return new UsersDTO(new ArrayList<>(users.getContent().stream().map(UserDTO::new).collect(Collectors.toList())), users.getTotalElements(), users.getTotalPages());
+    }
+
+    public UserProfileDTO getProfile(Long id) {
+        User user = usersRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found"));
+        if(user.getContacts() == null) {
+            user.setContacts(new Contacts(null, user, "", "", "", "", "", "", "", ""));
+        }
+        return new UserProfileDTO(user);
     }
 }
