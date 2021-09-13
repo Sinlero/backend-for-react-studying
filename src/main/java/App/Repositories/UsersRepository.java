eabd8790261540b1d72ba5e375e72f7c5de830fc
Sinlero <wanted1345@gmail.com>
2021-09-13 19:42:40 +0900

    add request to get user by login

--- src/main/java/App/Repositories/UsersRepository.java
+++ src/main/java/App/Repositories/UsersRepository.java
@@ -2,9 +2,13 @@ package App.Repositories;
 
 import App.Entities.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface UsersRepository extends PagingAndSortingRepository<User, Long> {
+
+    Optional<User> findUserByLogin(String login);
 
 }
