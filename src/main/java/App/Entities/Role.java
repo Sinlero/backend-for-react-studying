25b54431a8766b7aa84f6b2fa207e472ee6c6b8a
Sinlero <wanted1345@gmail.com>
2021-09-13 19:41:05 +0900

    add role entity

---
+++ src/main/java/App/Entities/Role.java
@@ -0,0 +1,15 @@
+package App.Entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
