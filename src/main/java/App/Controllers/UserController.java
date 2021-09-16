cf71a7d8aebbc6fc262f037377931e6e63e053d8
goldfinch_p8a <wanted1345@gmail.com>
2021-09-16 17:52:14 +0900

    Add responses for spring doc

--- src/main/java/App/Controllers/UserController.java
+++ src/main/java/App/Controllers/UserController.java
@@ -2,9 +2,11 @@ package App.Controllers;
 
 import App.DTO.UserProfileDTO;
 import App.DTO.UsersDTO;
-import App.Entities.User;
-import App.Exceptions.BigAskingException;
 import App.Services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,6 +31,10 @@ public class UserController {
 
     @GetMapping()
     @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Get list of users", description = "page > 1; 100 > limit > 1")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Well done", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "NONONONONO")})
     public UsersDTO getUsers(@RequestParam (defaultValue = "1", required = false) @Min(1) int page,
                              @RequestParam(defaultValue = "10", required = false) @Min(1) @Max(100) int limit) {
         log.info("Request page number: " + page + "; Request limit records: " + limit + "; Request number " + counter.incrementAndGet());
@@ -37,6 +43,10 @@ public class UserController {
 
     @GetMapping("/profile/{id}")
     @ResponseStatus(code = HttpStatus.OK)
+    @Operation(summary = "Get profile of user", description = "GIMMEIDUSER", method = "GEEEEEEET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Well done"),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "text/plain"))})
     public UserProfileDTO getProfile(@PathVariable Long id) {
         log.info("Request user with id " + id);
         return userService.getProfile(id);
