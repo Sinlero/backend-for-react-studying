68522f9ca62f5d9f248567d773eae6dbca29f543
goldfinch_p8a <wanted1345@gmail.com>
2021-09-25 14:04:28 +0900

    refactoring

--- src/main/java/App/Controllers/UserController.java
+++ src/main/java/App/Controllers/UserController.java
@@ -3,18 +3,10 @@ package App.Controllers;
 import App.DTO.UserProfileDTO;
 import App.DTO.UsersDTO;
 import App.Services.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import java.util.concurrent.atomic.AtomicLong;
 
 
